dee24c833dfed1ff374a0079931b4baf34ef1a5d
Crushedkrz78 <crispmolinito93@gmail.com>
2022-02-28 17:11:07 -0600

    Added template for the prototype

---
+++ sftp-sender.py
@@ -0,0 +1,19 @@
+"""
+This Script is intended to generate a class which will read a file and send it to a remote server
+through SFTP, using a customizable Server address, user and password.
+"""
+import pysftp
+
+class sftpManager:
+    def __init__():
+        self.hostName = ""
+        self.userName = ""
+        self.password = ""
+        self.destinationPath = ""
+        self.sourcePath = ""
+        
+    def listLocalFiles():
+        pass
+    
+    def sendFile():
+        pass
