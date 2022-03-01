97fa00ab32c6331b9864aa1f43a6402e8e9562e0
Crushedkrz78 <crispmolinito93@gmail.com>
2022-03-01 12:50:32 -0600

    Updted Sender function

--- sftp-sender.py
+++ sftp-sender.py
@@ -5,15 +5,22 @@ through SFTP, using a customizable Server address, user and password.
 import pysftp
 
 class sftpManager:
-    def __init__():
+    def __init__(self):
         self.hostName = ""
         self.userName = ""
         self.password = ""
         self.destinationPath = ""
         self.sourcePath = ""
-        
-    def listLocalFiles():
-        pass
     
-    def sendFile():
-        pass
+    def sendFile(self):
+        cnopts = pysftp.CnOpts()
+        cnopts.hostkeys = None
+        with pysftp.Connection(host=self.hostName, username=self.userName, password=self.password, cnopts=cnopts) as sftp:
+            print("Connection succesfully stablished ...")
+            
+            sftp.put_r(self.sourcePath, self.destinationPath)
+            
+    
+if __name__ == "__main__":
+    sendFileSFTP = sftpManager()
+    sendFileSFTP.sendFile()
